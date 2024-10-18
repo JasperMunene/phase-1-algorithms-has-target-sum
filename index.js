f5eff25af60e40e7341cf0c19d66aac509538d37
JasperMunene <jaspermunene2022@gmail.com>
2024-10-18 09:50:37 +0300

    Complete

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); 
+
+  for (const number of array) {   
+    const complement = target - number; 
+
+    if (seenNumbers.has(complement)) { 
+      return true; 
+    }
+
+    seenNumbers.add(number);  
+  }
+
+  return false;  // No pair found
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
+  Time complexity: O(n), where n is the length of the array.
+  - We loop through the array once, and checking for the presence of a number in the set (and adding to the set) are O(1) operations.
 
-/* 
-  Add your pseudocode here
+  Space complexity: O(n), where n is the number of elements in the array.
+  - In the worst case, we store all numbers in the set.
 */
 
 /*
-  Add written explanation of your solution here
+  Additional test cases:
 */
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10)); 
 
   console.log("");
 
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));  
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("=>", hasTargetSum([1, 2, 5], 4));  
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 3, 7], 6));  
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([5, 1, 3, 9], 20)); 
 }
 
 module.exports = hasTargetSum;
